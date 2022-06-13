3f16fe49e6a5990eec57da8df5062fa6a2445ace
Guo Tao <goto99@gmail.com>
2022-06-13 17:05:51 +0800

    Best result, removed the conditional test on thousands.

--- p12_int_to_roman.py
+++ p12_int_to_roman.py
@@ -1,52 +1,40 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        res = ""
+        thousands = num // 1000
+        hundreds = (num % 1000) // 100
+        tens = (num % 100) // 10
+        ones = num % 10 
 
-        def get_thousands():
-            thousands = num // 1000
-            return "M" * thousands
+        res = "M" * thousands
 
-        def get_hundreds():
-            hundreds = (num % 1000) // 100            
-            if hundreds == 9:
-                return "CM"
-            elif hundreds >= 5:
-                return "D" + "C" * (hundreds - 5)
-            elif hundreds == 4:
-                return "CD"
-            else:
-                return "C" * hundreds
-        def get_tens():
-            tens = (num % 100) // 10
-            if tens == 9:
-                return "XC"
-            elif tens >= 5:
-                return "L" + "X" * (tens - 5)
-            elif tens == 4:
-                return "XL"
-            else:
-                return "X" * tens
-        def get_ones():
-            ones = num % 10
-            if ones == 9:
-                return "IX"
-            elif ones >=5:
-                return "V" + "I" * (ones - 5)
-            elif ones ==4:
-                return "IV"
-            else:
-                return "I" * ones
-
-        if num > 999:
-            res += get_thousands() + get_hundreds() + get_tens() + get_ones()
-        elif num > 99:
-            return get_hundreds() + get_tens() + get_ones()
-        elif num > 9:
-            return get_tens() + get_ones()
+        if hundreds == 9:
+            res += "CM"
+        elif hundreds >= 5:
+            res += "D" + "C" * (hundreds - 5)
+        elif hundreds == 4:
+            res += "CD"
         else:
-            return get_ones()
-def main():
-    s = Solution()
-    print(s.intToRoman(1994))
+            res += "C" * hundreds
+
+        if tens == 9:
+            res += "XC"
+        elif tens >= 5:
+            res += "L" + "X" * (tens -5)
+        elif tens == 4:
+            res += "XL"
+        else:
+            res += "X" * tens
+        
+        if ones == 9:
+            res += "IX"
+        elif ones >=5:
+            res += "V" + "I" * (ones - 5)
+        elif ones ==4:
+            res += "IV"
+        else:
+            res += "I" * ones
+        
+        return res    
+
 
 
